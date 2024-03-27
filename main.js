58c13ffb0fee685d17a89eb22d046c9d7d62347b
tatumal <110982968+tatumal@users.noreply.github.com>
2024-03-27 08:57:55 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,24 @@
+<script src="main.js"></script>
+const botoes = document.querySelectorAll(".botao");
+console.log(botoes)
+for(let i=0; i <  botoes.length; i++){
+    console.log(i);
+}
+botoes[i].onclick = function(){
+    
+    botoes[i].classList.add("ativo");
+}
+for(let j=0;j<botoes.length;j++){
+    botoes[j].classList.remove("ativo");
+}
+const botoes = document.querySelectorAll(".botao");
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function(){
+
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo");
+    }
+}

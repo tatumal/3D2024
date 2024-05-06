7c342747caca6a50dae833652117554293ed54ca
tatumal <110982968+tatumal@users.noreply.github.com>
2024-05-06 09:03:10 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ for (let i = 0; i < botoes.length; i++) {
     }
 }
 
+
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-06-11T00:00:00");
 const tempoObjetivo2 = new Date("2024-12-05T00:00:00");
@@ -21,6 +22,9 @@ const tempoObjetivo3 = new Date("2024-12-30T00:00:00");
 const tempoObjetivo4 = new Date("2024-09-01T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
+contadores[0].textContent = calculaTempo(tempos[0]);
+
 
 
 function calculaTempo(tempoObjetivo) {
